5e8847c83a24a27eef3e19f6bd4428d49d9a27da
nithin pradeep <nithinpradeep@nithins-MacBook-Pro.local>
2024-07-21 17:59:53 -0700

    Retrieval and generation added

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="qasystem",
+    version="0.0.1",
+    author="Nithin",   
+    author_email="nithinpradeep38@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain","langchainhub","bs4","tiktoken","openai","boto3","langchain_community","chromadb","awscli",
+"streamlit",
+"pypdf",
+"faiss-cpu"]
+)
